63504adb885575a8f435da8068e39114c65a05e6
sami <sami-karem@outlook.fr>
2023-06-20 22:37:28 +0100

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,38 +1,21 @@
 #include "main.h"
 
 /**
- * put_char - printing _putchar.
- * parameterstr: where we store '_putchar'
+ * main - printing _putchar.
  *
- * Return: always 0.
-*/
-
-void	put_char(char *str)
-{
-	int	i;
-
-	str = "_putchar";
-	i = 0;
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
-}
-
-/**
- * main - main of put_char.
  * Return: always 0.
 */
 
 int main(void)
 {
-	char	*str;
-
-	str = 0;
-	put_char(str);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
-
 }
