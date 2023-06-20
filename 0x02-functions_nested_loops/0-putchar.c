7688378c318211a7ea19838484451cfaeb011ec4
sami <sami-karem@outlook.fr>
2023-06-20 22:02:37 +0100

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,26 @@
  * Return: always 0.
 */
 
+void	put_char(char *str)
+{
+	int	i;
+
+	str = "_putchar";
+	i = 0;
+	while (i <= '\n')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	write(1, '\n', 1);
+}
+
 int main(void)
 {
-	_putchar('_putchar');
-	_putchar('\n');
+	char	*c;
+
+	c = 0;
+	put_char(c);
 	return (0);
+
 }
