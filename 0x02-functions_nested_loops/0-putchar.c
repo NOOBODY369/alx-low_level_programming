3bda553f6f6e469a93e6ca69dd79db4d9c3a77c4
sami <sami-karem@outlook.fr>
2023-06-20 21:43:07 +0100

    loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ *  main - printing _putchar.
+ *
+ * Return: always 0.
+*/
+
+int	_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+void	put_char(char *str)
+{
+	int	i;
+
+	str = "_putchar";
+	i = 0;
+	while (i <= '\n')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
+int main(void)
+{
+	char	*c;
+
+	c = 0;
+	put_char(c);
+	return (0);
+
+}
