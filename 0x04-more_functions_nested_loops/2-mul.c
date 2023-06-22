4b9c4e071772b164c25c9caf50e5721f28d47710
sami <sami-karem@outlook.fr>
2023-06-22 18:37:38 +0100

    more

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers.
+ * @a:character 1.
+ * @b: character 2.
+ *
+ * Return: the result.
+*/
+
+int	mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - fizzbuzz
+ *
+ * return: always 0.
+*/
+
+int main(void)
+{
+	int	i = 1;
+
+	while (i <= 100)
+	{
+		if ((i % 3 == 0 && i % 5== 0))
+			printf("fizzbuzz");
+		else if (i % 3 == 0)
+			printf("fizz");
+		else if (i % 5 == 0)
+			printf("buzz");
+		else
+			printf("%d", i);
+		i++;
+		printf(" ");
+	}
+	printf("\n");
+}
