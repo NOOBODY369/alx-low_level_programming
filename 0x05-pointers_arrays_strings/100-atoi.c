9ae3d53dbccc6412477e9eaea182c133675b89b2
sami <sami-karem@outlook.fr>
2023-06-26 23:02:01 +0100

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,7 +22,7 @@ int _atoi(char *s)
 			sign *= -1;
 	while (s[i] == '+' || s[i] == '-')
 		i++;
-	while (s[i] >= '0' && s[i] <= '9')
+	while (s[i] && s[i] >= '0' && s[i] <= '9')
 	{
 		nb = nb * 10 + (s[i] - '0');
 		i++;
