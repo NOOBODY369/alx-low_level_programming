ce664c6cf6a439a9be7e4ae30d8b601f0fc0c269
sami <sami-karem@outlook.fr>
2023-06-26 23:06:06 +0100

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,23 +9,21 @@
 
 int _atoi(char *s)
 {
-	int				i;
+	int				res;
 	int				sign;
-	unsigned int	nb;
 
-	i = 0;
 	sign = 1;
-	nb = 0;
-	while (s[i] &&(s[i] == ' ' || (s[i] >= 9 && s[i] <= 13)))
-		++i;
-	if (s[i] == '-')
-			sign *= -1;
-	while (s[i] == '+' || s[i] == '-')
-		i++;
-	while (s[i] && s[i] >= '0' && s[i] <= '9')
+	res = 0;
+	while (*s &&(*s == ' ' || (*s >= 9 && *s <= 13)))
+		++s;
+	if (*s == '-')
+			sign = -1;
+	if (*s == '+' || *s == '-')
+		++s;
+	while (*s && *s >= '0' && *s <= '9')
 	{
-		nb = nb * 10 + (s[i] - '0');
-		i++;
+		res = res * 10 + (*s - '0');
+		++s;
 	}
-	return (sign * (int)nb);
+	return (sign * res);
 }
