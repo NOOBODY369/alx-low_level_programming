5e078e160679b7190592e74fd8d8af24adddf8d7
sami <sami-karem@outlook.fr>
2023-07-07 21:22:52 +0100

    args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,52 @@
 #include "main.h"
 
 /**
+ * _atoi - function that convert a string to an integer.
+ * @s:	string to convert.
+ *
+ * Return: string converted.
+*/
+
+int	_atoi(char *s)
+{
+	int	i,	d,	n,	len,	f,	digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+	len++;
+
+	while (i < len && f == 0)
+	{
+	if (s[i] == '-')
+	++d;
+
+	if (s[i] >= '0' && s[i] <= '9')
+	{
+	digit = s[i] - '0';
+	if (d % 2)
+	digit = -digit;
+	n = n * 10 + digit;
+	f = 1;
+	if (s[i + 1] < '0' || s[i + 1] > '9')
+	break;
+	f = 0;
+	}
+	i++;
+	}
+
+	if (f == 0)
+	return (0);
+
+	return (n);
+}
+
+/**
  * main - program that multiplies two numbers.
  * @argv: argument values.
  * @argc: number of argument.
@@ -18,7 +64,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	i = atoi(argv[1]) * atoi(argv[2]);
+	i = _atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", i);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,6 @@
 
 #include <stdio.h>
 
-
+int	_atoi(char *s);
 
 #endif
