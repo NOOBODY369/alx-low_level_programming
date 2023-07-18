a73106a0b6a787f12faacfe3b7f45132bc362062
sami <sami-karem@outlook.fr>
2023-07-18 16:39:13 +0100

    struct\process

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,23 @@
 #ifndef _HEADER_
 #define _HEADER_
 
+/**
+ * struct dog - Dog informations
+ * @name: pointer pointed to the name of dog
+ * @age: pointer pointed to the age of dog
+ * @owner: pointer pointed to the owner of dog
+ */
 struct dog
 {
 		char	*name;
 		float	age;
 		char	*owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+
 typedef struct dog dog_t;
 
 void	init_dog(struct dog *d, char *name, float age, char *owner);
