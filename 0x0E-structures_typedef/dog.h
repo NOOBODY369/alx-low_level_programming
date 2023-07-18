e9fa041316996363e6ae14bce1327e9d4f13ac03
sami <sami-karem@outlook.fr>
2023-07-18 16:15:18 +0100

    struct\process

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct dog
+{
+	char	*name;
+	float	age;
+	char	*owner;
+};
+
+void	init_dog(struct dog *d, char *name, float age, char *owner);
+void	print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+void	free_dog(dog_t *d);
+
+#endif
