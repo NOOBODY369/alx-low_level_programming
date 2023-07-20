5b67b9b5250e50ba3150018ff315dbd729580471
sami <sami-karem@outlook.fr>
2023-07-20 01:27:03 +0100

    pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,6 +16,8 @@ int	int_index(int *array, int size, int (*cmp)(int))
 {
 	int	index;
 
+	if (size <= 0)
+		return (-1);
 	if (array == NULL || cmp == NULL)
 		return (-1);
 	for (index == 0; index < size; index++)
@@ -23,5 +25,4 @@ int	int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[index]) != 0)
 			return (index);
 	}
-	return (-1);
 }
