d7b1376ca87da3703c1716733272c1acfe302fa9
sami <sami-karem@outlook.fr>
2023-09-01 00:22:41 +0100

    bit man

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+void			print_binary(unsigned long int n);
 unsigned int	binary_to_uint(const char *b);
 int				get_bit(unsigned long int n, unsigned int index);
 int				set_bit(unsigned long int *n, unsigned int index);
