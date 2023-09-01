87ebfed7bc9de52a837790cb6b36932c3c4c0cb6
sami <sami-karem@outlook.fr>
2023-09-01 01:03:48 +0100

    bit manip

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary
+ * number to an unsigned int.
+ * @b: pointer to a string of 0 and 1 chars
+ *
+ * Return: the converted number, or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+*/
+
+unsigned int	binary_to_uint(const char *b)
+{
+	unsigned int	sum;
+	int				i;
+
+	sum = 0;
+	i = 0;
+	if (!b)
+		return (0);
+	while (b[i])
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			sum = (sum << 1) + (b[i] - '0');
+			i++;
+		}
+		else
+			return (0);
+	}
+	return (sum);
+}
