550e58336318b4ac52cb36329e98740e5c9ddbc5
sami <sami-karem@outlook.fr>
2023-11-23 19:14:18 +0100

    manip

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,12 +6,12 @@
  * Return: 0 if big endian, 1 if little endian.
 */
 
-int get_endianness(void)
+int	get_endianness(void)
 {
 	char			*c;
 	unsigned int	i;
 
 	i = 1;
-	c = (char *)&i;
+	c = (char *) &i;
 	return ((int)*c);
 }
